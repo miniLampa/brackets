1f32a0b4786ba2e2be2c04c0c12f9a23baf27488
Natalia Litvinok <minilampa@gmail.com>
2019-03-05 19:16:28 +0300

    feat: add checking if brackets sequence is correct

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stringResult = str;
+  let foundNext;
+
+  bracketsConfig.forEach(function (item) {
+    item[0] = item[0].replace(/(([^\\]|^)[\(\)\[\]\.\+\^\$\*\?\{\}\|])/g, "\\$1");
+    item[1] = item[1].replace(/(([^\\]|^)[\(\)\[\]\.\+\^\$\*\?\{\}\|])/g, "\\$1");
+  });
+
+  do {
+    foundNext = false;
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let regExp = new RegExp(bracketsConfig[i][0] + bracketsConfig[i][1]);
+      foundNext = foundNext || stringResult.search(regExp) >= 0;
+      stringResult = stringResult.replace(regExp, "");
+    }
+
+  } while (foundNext)
+
+  return !stringResult.length;
 }
